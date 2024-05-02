03e42a9eb6ee5fb2cfec919b73943fafd3046d50
Gasta Marc Oliver <marcolivergastagonzales@gmail.com>
2024-05-02 17:09:25 +0800

    update all files

--- time.js
+++ time.js
@@ -1,4 +1,3 @@
-
 let count = 0;
 let intervalId;
 
@@ -8,35 +7,35 @@ let resumeButton = document.getElementById("resume");
 let stopButton = document.getElementById("stop");
 let countSpan = document.getElementById("count");
 
-startButton.addEventListener("click", function() {
-    intervalId = setInterval(function() {
-        count++;
-        countSpan.textContent = count;
-    }, 1000);
-    startButton.disabled = true;
-    pauseButton.disabled = false;
-});
-
-pauseButton.addEventListener("click", function() {
-    clearInterval(intervalId);
-    pauseButton.disabled = true;
-    resumeButton.disabled = false;
-});
-
-resumeButton.addEventListener("click", function() {
-    intervalId = setInterval(function() {
-        count++;
-        countSpan.textContent = count;
-    }, 1000);
-    pauseButton.disabled = false;
-    resumeButton.disabled = true;
-});
-
-stopButton.addEventListener("click", function() {
-    clearInterval(intervalId);
-    count = 0;
+startButton.addEventListener("click", function () {
+  intervalId = setInterval(function () {
+    count++;
     countSpan.textContent = count;
-    startButton.disabled = false;
-    pauseButton.disabled = true;
-    resumeButton.disabled = true;
+  }, 1000);
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+});
+
+pauseButton.addEventListener("click", function () {
+  clearInterval(intervalId);
+  pauseButton.disabled = true;
+  resumeButton.disabled = false;
+});
+
+resumeButton.addEventListener("click", function () {
+  intervalId = setInterval(function () {
+    count++;
+    countSpan.textContent = count;
+  }, 1000);
+  pauseButton.disabled = false;
+  resumeButton.disabled = true;
+});
+
+stopButton.addEventListener("click", function () {
+  clearInterval(intervalId);
+  count = 0;
+  countSpan.textContent = count;
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+  resumeButton.disabled = true;
 });
